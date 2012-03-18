cd7faf5c91a1c1dc8fafa14b209979cc2f5869a9
Tim Rayburn <tim@timrayburn.net>
2012-03-18 01:38:01 -0500

    One line parse working

--- Parser.cs
+++ Parser.cs
@@ -3,73 +3,154 @@ using System.Collections.Generic;
 
 namespace SWTOR.Parser
 {
-	public class Parser
-	{
-		public IList<LogEntry> Parse(System.IO.TextReader rdr)
-		{
-			if (rdr == null) throw new ArgumentNullException("rdr");
-			
-			string line = null;
-			var list = new List<LogEntry>();
-			
-			while ((line = rdr.ReadLine()) != null)
-			{
-				var entry = new LogEntry();
-				
-				int start = line.IndexOf('[') + 1;
-				int end = line.IndexOf(']',start);
-				
-				string tStamp = line.Substring(start,end-start);
-				entry.Timestamp = Convert.ToDateTime(tStamp);
-				
-				var rest = line.Substring(end);
-				
-				start = rest.IndexOf('[') + 1;
-				end = rest.IndexOf(']',start);
-				
-				var source = rest.Substring(start,end-start);
-				entry.Source = new Actor();
-				entry.Source.IsPlayer = source.StartsWith("@");
-				entry.Source.Name = source.Replace("@","");
-				
-				rest = rest.Substring(end);
-				
-			 	list.Add(entry);
-			}
-			
-			return list;
-		}
-		
-		private BetweenResult Between(char startChar, char endChar, string line)
-		{
-			var result = new BetweenResult();
-			
-			int start = line.IndexOf(startChar) + 1;
-			int end = line.IndexOf(endChar,start);
-			
-			result.FoundValue = line.Substring(start,end-start);
-			result.Rest = line.Substring(end);
-			
-		}
-		
-		class BetweenResult
-		{
-			public string FoundValue {get;set;}
-			public string Rest {get;set;}
-		}
-	}
-	
-	public class LogEntry
-	{
-		public DateTime Timestamp {get;set;}
-		public Actor Source {get;set;}
-		public Actor Target {get;set;}
-	}
-	
-	public class Actor
-	{
-		public string Name {get;set;}
-		public bool IsPlayer {get;set;}
-	}
+    public class Parser
+    {
+        public IList<LogEntry> Parse(System.IO.TextReader rdr)
+        {
+            if (rdr == null) throw new ArgumentNullException("rdr");
+
+            string line = null;
+            var list = new List<LogEntry>();
+
+            while ((line = rdr.ReadLine()) != null)
+            {
+                var entry = new LogEntry();
+                string rest = null;
+
+                var btwn = Between('[', ']', line);
+                entry.Timestamp = Convert.ToDateTime(btwn.FoundValue);
+
+                rest = ParseSourceAndTarget(entry, btwn.Rest);
+
+                rest = ParseAbility(entry, rest);
+
+                rest = ParseEvent(entry, rest);
+
+                rest = ParseEventAmount(entry, rest);
+
+                list.Add(entry);
+            }
+
+            return list;
+        }
+
+        private string ParseEventAmount(LogEntry entry, string line)
+        {
+            //  ()
+            var btwn = Between('(', ')', line);
+            if (string.IsNullOrWhiteSpace(btwn.FoundValue) == false)
+                entry.Event.Amount = Convert.ToInt32(btwn.FoundValue);
+            else entry.Event.Amount = 0;
+            return btwn.Rest;
+        }
+
+        private string ParseEvent(LogEntry entry, string line)
+        {
+            // [ApplyEffect {836045448945477}: Stunned (Physical) {2848585519464704}]
+            var btwn = Between('[', ']', line);
+            var splitFound = btwn.FoundValue.Split(':');
+            var rest = btwn.Rest;
+
+            btwn = Between('{', '}', splitFound[0]);
+            entry.Event.Name = btwn.BeforeFound.Trim();
+            entry.Event.Number = Convert.ToInt64(btwn.FoundValue);
+
+            btwn = Between('{', '}', splitFound[1]);
+            entry.Event.EffectNumber = Convert.ToInt64(btwn.FoundValue);
+            btwn = Between('(', ')', btwn.BeforeFound);
+            entry.Event.EffectName = btwn.BeforeFound.Trim();
+            entry.Event.EffectSubtype = btwn.FoundValue.Trim();
+
+            return rest;
+        }
+        private string ParseAbility(LogEntry entry, string line)
+        {
+            // [Force Clap {2848585519464448}]
+            var btwn = Between('[', ']', line);
+            var rest = btwn.Rest;
+            btwn = Between('{', '}', btwn.FoundValue);
+            entry.Ability.Name = btwn.BeforeFound.Trim();
+            entry.Ability.Number = Convert.ToInt64(btwn.FoundValue);
+            return rest;
+        }
+
+        private string ParseSourceAndTarget(LogEntry entry, string line)
+        {
+            var btwn = Between('[', ']', line);
+            entry.Source.IsPlayer = btwn.FoundValue.StartsWith("@");
+            entry.Source.Name = btwn.FoundValue.Replace("@", "");
+
+            btwn = Between('[', ']', btwn.Rest);
+            entry.Target.IsPlayer = btwn.FoundValue.StartsWith("@");
+            entry.Target.Name = btwn.FoundValue.Replace("@", "");
+
+            return btwn.Rest;
+        }
+
+        private BetweenResult Between(char startChar, char endChar, string line)
+        {
+            var result = new BetweenResult();
+
+            result.StartOfFound = line.IndexOf(startChar) + 1;
+            result.EndOfFound = line.IndexOf(endChar, result.StartOfFound);
+
+            result.FoundValue = line.Substring(result.StartOfFound, result.EndOfFound - result.StartOfFound);
+            result.Rest = line.Substring(result.EndOfFound);
+            result.BeforeFound = line.Substring(0, result.StartOfFound - 1);
+            result.Original = line;
+
+            return result;
+        }
+
+        class BetweenResult
+        {
+            public string FoundValue { get; set; }
+            public string Rest { get; set; }
+            public string Original { get; set; }
+            public string BeforeFound { get; set; }
+            public int StartOfFound { get; set; }
+            public int EndOfFound { get; set; }
+        }
+    }
+
+    public class LogEntry
+    {
+        public LogEntry()
+        {
+            Source = new Actor();
+            Target = new Actor();
+            Event = new Event();
+            Ability = new Ability();
+        }
+
+        public DateTime Timestamp { get; set; }
+        public Actor Source { get; set; }
+        public Actor Target { get; set; }
+        public Event Event { get; set; }
+        public Ability Ability { get; set; }
+    }
+
+    public class Ability
+    {
+        public string Name { get; set; }
+        public Int64 Number { get; set; }
+    }
+
+    public class Actor
+    {
+        public string Name { get; set; }
+        public bool IsPlayer { get; set; }
+    }
+
+    public class Event
+    {
+        public string Name { get; set; }
+        public Int64 Number { get; set; }
+        public string EffectName { get; set; }
+        public string EffectSubtype { get; set; }
+        public Int64 EffectNumber { get; set; }
+        public int Amount { get; set; }
+        public int Threat { get; set; }
+    }
 }
 
--- SWTOR.Parser.Tests/ParserTests.cs
+++ SWTOR.Parser.Tests/ParserTests.cs
@@ -2,81 +2,143 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Text;
-using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SWTOR.Parser.Tests
 {
-	[TestFixture()]
-	public class OneRow_ParserTests
-	{
-		private StringBuilder oneRow;
-		private Parser target;
-		private StringReader rdr;
-		
-		[SetUp]
-		public void SetUp()
-		{
-			target = new Parser();
-			oneRow = new StringBuilder();
-			oneRow.Append("[03/01/2012 14:35:20] [@Idrurrez] [@Khantni] " + 
-			              "[Force Clap {2848585519464448}] " + 
-			              "[ApplyEffect {836045448945477}: Stunned (Physical) {2848585519464704}] ()");
-			rdr = new StringReader(oneRow.ToString());
-		}
-		
-		[Test]
-		public void Parse_Should_Return_Player_Idrurrez_As_Source()
-		{
-			// Arrange
-			
-			// Act
-			var list = target.Parse(rdr);
-			
-			// Assert
-			var entry = list.First();
-			Assert.AreEqual("Idrurrez", entry.Source.Name);
-			Assert.AreEqual(true, entry.Source.IsPlayer);
-		}
-		
-				[Test()]
-		public void Parse_Should_Return_One_Entry()
-		{
-			// Arrange
-			
-			// Act
-			var list = target.Parse(rdr);
-			
-			// Assert
-			Assert.AreEqual(1,list.Count);
-		}
-		
-		[Test]
-		public void Parse_Should_Return_Correct_Timestamp()
-		{
-			// Arrange
-			
-			// Act
-			var list = target.Parse(rdr);
-			
-			// Assert
-			var entry = list.First();
-			var expected = new DateTime(2012,3,1,14,35,20,DateTimeKind.Unspecified);
-			Assert.AreEqual(expected, entry.Timestamp);
-		}
-		
-		[Test()]
-		[ExpectedException(typeof(ArgumentNullException))]
-		public void Parse_Should_Throw_When_TextReader_Is_Null ()
-		{
-			// Arrange
-			
-			// Act
-			var list = target.Parse(null);
-			
-			// Assert
-			Assert.IsNull(list);
-			Assert.Fail();
-		}		
-	}
+    [TestClass]
+    public class OneRow_ParserTests
+    {
+        private StringBuilder oneRow;
+        private Parser target;
+        private StringReader rdr;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            target = new Parser();
+            oneRow = new StringBuilder();
+            oneRow.Append("[03/01/2012 14:35:20] [@Idrurrez] [@Khantni] " +
+                          "[Force Clap {2848585519464448}] " +
+                          "[ApplyEffect {836045448945477}: Stunned (Physical) {2848585519464704}] ()");
+            rdr = new StringReader(oneRow.ToString());
+        }
+
+        [TestMethod]
+        public void Parse_Should_Return_Zero_Event_Amount()
+        {
+            // Arrange
+            
+
+            // Act
+            var list = target.Parse(rdr);
+
+            // Assert
+            var entry = list.First();
+            Assert.AreEqual(entry.Event.Amount, 0);
+        }
+
+        [TestMethod]
+        public void Parse_Should_Return_ForceClap_Ability()
+        {
+            // Arrange
+            
+
+            // Act
+            var list = target.Parse(rdr);
+
+            // Assert
+            var entry = list.First();
+            Assert.AreEqual(entry.Ability.Name, "Force Clap");
+            Assert.AreEqual(entry.Ability.Number, 2848585519464448);
+        }
+
+        [TestMethod]
+        public void Parse_Should_Return_ApplyEffect_Event()
+        {
+            // Arrange
+            
+
+            // Act
+            var list = target.Parse(rdr);
+
+            // Assert
+            var entry = list.First();
+            Assert.AreEqual(entry.Event.Name, "ApplyEffect");
+            Assert.AreEqual(entry.Event.Number, 836045448945477);
+            Assert.AreEqual(entry.Event.EffectName, "Stunned");
+            Assert.AreEqual(entry.Event.EffectSubtype, "Physical");
+            Assert.AreEqual(entry.Event.EffectNumber, 2848585519464704);
+        }
+
+        [TestMethod]
+        public void Parse_Should_Return_Player_Khantni_As_Target()
+        {
+            // Arrange
+            
+
+            // Act
+            var list = target.Parse(rdr);
+
+            // Assert
+            var entry = list.First();
+            Assert.AreEqual("Khantni", entry.Target.Name);
+            Assert.AreEqual(true, entry.Target.IsPlayer);
+        }
+
+        [TestMethod]
+        public void Parse_Should_Return_Player_Idrurrez_As_Source()
+        {
+            // Arrange
+
+            // Act
+            var list = target.Parse(rdr);
+
+            // Assert
+            var entry = list.First();
+            Assert.AreEqual("Idrurrez", entry.Source.Name);
+            Assert.AreEqual(true, entry.Source.IsPlayer);
+        }
+
+        [TestMethod]
+        public void Parse_Should_Return_One_Entry()
+        {
+            // Arrange
+
+            // Act
+            var list = target.Parse(rdr);
+
+            // Assert
+            Assert.AreEqual(1, list.Count);
+        }
+
+        [TestMethod]
+        public void Parse_Should_Return_Correct_Timestamp()
+        {
+            // Arrange
+
+            // Act
+            var list = target.Parse(rdr);
+
+            // Assert
+            var entry = list.First();
+            var expected = new DateTime(2012, 3, 1, 14, 35, 20, DateTimeKind.Unspecified);
+            Assert.AreEqual(expected, entry.Timestamp);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Parse_Should_Throw_When_TextReader_Is_Null()
+        {
+            // Arrange
+
+            // Act
+            var list = target.Parse(null);
+
+            // Assert
+            Assert.IsNull(list);
+            Assert.Fail();
+        }
+    }
 }
 
