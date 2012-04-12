50fdfc472589a7a70ecd5fda53ffa2723b6ea793
Tim Rayburn <tim@timrayburn.net>
2012-04-12 18:05:37 -0500

    Ok fine Transient

--- src/SWTOR.Web/IoC/RavenInstaller.cs
+++ src/SWTOR.Web/IoC/RavenInstaller.cs
@@ -36,7 +36,7 @@ namespace SWTOR.Web.IoC
                     docStore.Configuration.Port = 8888;
                     docStore.Initialize();
                     return docStore;
-                }).LifeStyle.PerWebRequest
+                }).LifeStyle.Transient
                 );
 #endif
 #if !DEBUG
@@ -51,7 +51,7 @@ namespace SWTOR.Web.IoC
                             DefaultQueryingConsistency = ConsistencyOptions.QueryYourWrites
                         }
                     }.Initialize();
-                }).LifeStyle.PerWebRequest
+                }).LifeStyle.Transient
                 );
 #endif
 
